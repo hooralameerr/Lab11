5263c01a25afaad77613176f3e5a6e21acad0e31
hoor <hoor@192.168.0.56>
2026-09-24 17:24:44 +0300

    modified if statement

--- mystring.java
+++ mystring.java
@@ -14,8 +14,8 @@ Temp = Text.toCharArray();
 Text= "";
 for ( i=0; i<n; i++) {
 if (Temp[i] >= 'A' && Temp[i] <= 'Z')
-Temp[i]= (char)(Temp[i] - 'A' + 'a') ;
-if (Temp[i] >= 'a' && Temp[i] <= 'z')
+	Temp[i] = 'A';
+else if (Temp[i] >= 'a' && Temp[i] <= 'z') 
 Temp[i]= (char)(Temp[i] - 'a' + 'A') ;
 
 Text += Temp[i];
